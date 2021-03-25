fba41ce2452ebfc2cbb029c7370221229cc6aa8e
Justin <304602555@qq.com>
2021-03-25 14:25:56 +0800

    关闭grpc

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
-	"mall_server/api"
-	"mall_server/pkg/rabbitmq/auto_close_order"
-	"mall_server/pkg/rabbitmq/consume_wx_pay_notice"
 	"mall_server/store"
 	"time"
 )
 
 func main() {
-	mq := new(store.Rabbitmq)
-	mq.Get()
+	//mq := new(store.Rabbitmq)
+	//mq.Get()
 	redis := new(store.Redis)
 	err := redis.Get().Ping().Err()
 	if err != nil {
@@ -21,10 +18,10 @@ func main() {
 	mysql.Get()
 	log := new(store.Log)
 	//go etcd.Register()
-	go api.GrpcServer()
+	//go api.GrpcServer()
 	log.Get().Debug("server started at %v", time.Now())
 	fmt.Printf("server started at %v \n", time.Now())
-	go consume_wx_pay_notice.Do()
-	go auto_close_order.Do()
+	//go consume_wx_pay_notice.Do()
+	//go auto_close_order.Do()
 	select {}
 }
